0b67e7e0b86e2d8fbfe1eb1be55db96669ed875b
Jonathan Soto <jonathansoto@MacBook-Pro-de-Jonathan.local>
2022-09-05 16:17:55 -0600

    upload

---
+++ area_triangulo.py
@@ -0,0 +1,12 @@
+print("Programa área triangulo")
+
+base = 0
+altura = 0
+result = 0
+
+base = int( input("Ingrese la base \n") )
+altura = int( input("Ingrese la altura\n") )
+
+result = base * altura / 2
+
+print("Resultado: ", result)
---
+++ promedio_3_num.py
@@ -0,0 +1,14 @@
+print("promedio 3 números")
+
+num1 = 0
+num2 = 0
+num3 = 0
+result= 0
+
+num1 = int( input("Ingrese la num1 \n") )
+num2 = int( input("Ingrese la num2\n") )
+num3 = int( input("Ingrese la num3\n") )
+
+result = (num1 + num2 + num3) / 3
+
+print("Resultado: ", result)
